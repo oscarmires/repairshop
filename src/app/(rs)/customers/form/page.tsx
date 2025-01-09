2f0edec8478071932597d5f291c6a32ab14b7a42
oscar-me <oscar-me@outlook.com>
2025-01-08 22:45:58 -0600

    Lesson 4 and 5

---
+++ src/app/(rs)/customers/form/page.tsx
@@ -0,0 +1,42 @@
+import { getCustomer } from "@/lib/queries/getCustomer";
+import { BackButton } from "@/components/BackButton";
+import CustomerForm from "@/app/(rs)/customers/form/CustomerForm";
+
+import * as Sentry from "@sentry/nextjs";
+
+export default async function CustomerFormPage({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+    try {
+        const { customerId } = await searchParams;
+
+        // Edit customer form
+        if (customerId) {
+            const customer = await getCustomer(parseInt(customerId));
+
+            if (!customer) {
+                return (
+                    <>
+                        <h2 className="text-2xl mb-2">
+                            Customer ID #{customerId} not found
+                        </h2>
+                        <BackButton title="Go Back" variant="default" />
+                    </>
+                );
+            }
+            console.log(customer);
+            // put customer form component
+            return <CustomerForm customer={customer} />;
+        } else {
+            // new customer form component
+            return <CustomerForm />;
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            Sentry.captureException(error);
+            throw error;
+        }
+    }
+}
